b804fc0f814f086433fee793fb4c09567f2f8661
FabricioLR <dimamitoyt@gmail.com>
2023-03-06 18:37:34 -0300

    update axios default headers

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,14 +1,15 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://cors.ryanking13.workers.dev/?u=https://mangalivre.net/",
+    baseURL: "https://mangalivre.net/",
     headers: {
         "HTTP_X_REQUESTED_WITH": "XMLHttpRequest",
         "X-Requested-With": "XMLHttpRequest",
         "Content-Type": "application/json",
         "sec-fetch-dest": "empty",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-        "refer": "localhost.com"
+        "refer": "localhost.com",
+        "Accept-Encoding": "gzip, deflate, br"
     }
 })
 
@@ -18,5 +19,7 @@ api.defaults.headers.common["Content-Type"] = "application/json"
 api.defaults.headers.common["sec-fetch-dest"] = "empty"
 api.defaults.headers.common["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 api.defaults.headers.common["refer"] = "localhost.com"
+api.defaults.headers.common["Accept-Encoding"] = "gzip, deflate, br"
+
 
 export { api }
