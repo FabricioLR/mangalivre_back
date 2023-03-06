4701e125c5cc15f108151fff440884c9f9c4d30c
FabricioLR <dimamitoyt@gmail.com>
2023-03-06 18:26:54 -0300

    add default headers to axios

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,22 @@
 import axios from "axios"
 
-export const api = axios.create({
-    baseURL: "https://mangalivre.net/"
+const api = axios.create({
+    baseURL: "https://mangalivre.net/",
+    headers: {
+        "HTTP_X_REQUESTED_WITH": "XMLHttpRequest",
+        "X-Requested-With": "XMLHttpRequest",
+        "Content-Type": "application/json",
+        "sec-fetch-dest": "empty",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+        "refer": "localhost.com"
+    }
 })
+
+api.defaults.headers.common["HTTP_X_REQUESTED_WITH"] = "XMLHttpRequest"
+api.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest"
+api.defaults.headers.common["Content-Type"] = "application/json"
+api.defaults.headers.common["sec-fetch-dest"] = "empty"
+api.defaults.headers.common["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+api.defaults.headers.common["refer"] = "localhost.com"
+
+export { api }
