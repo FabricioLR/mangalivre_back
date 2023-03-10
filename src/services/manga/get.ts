24c2f7c669b8d6b2f0f5846983c9c53babf30e2c
FabricioLR <dimamitoyt@gmail.com>
2023-03-10 15:08:56 -0300

    revert child process test

--- src/services/manga/get.ts
+++ src/services/manga/get.ts
@@ -1,6 +1,5 @@
 import { Manga } from "../../types/types"
 import { api } from "../axios"
-import { exec } from "child_process"
 
 type MangasData = {
     dia: Manga[]
@@ -11,20 +10,14 @@ type MangasData = {
 
 export const get = async () => {
     try {
-        // const mangas: MangasData = {
-        //     dia: await (await api.get("home/most_read_period?type=manga&period=day")).data.most_read,
-        //     semana: await (await api.get("home/most_read_period?type=manga&period=week")).data.most_read,
-        //     mês: await (await api.get("home/most_read_period?type=manga&period=month")).data.most_read,
-        //     ano: await (await api.get("home/most_read_period?type=manga&period=year")).data.most_read
-        // }
+        const mangas: MangasData = {
+            dia: await (await api.get("home/most_read_period?type=manga&period=day")).data.most_read,
+            semana: await (await api.get("home/most_read_period?type=manga&period=week")).data.most_read,
+            mês: await (await api.get("home/most_read_period?type=manga&period=month")).data.most_read,
+            ano: await (await api.get("home/most_read_period?type=manga&period=year")).data.most_read
+        }
 
-        // return mangas
-        exec("curl https://mangalivre.net/home/most_read_period?type=manga&period=day", (error, stdout, stderr) => {
-            console.log(error)
-            console.log(stdout)
-        })
-
-        return {}
+        return mangas
     } catch (error: any) {
         console.log("manga get error", error)
         throw error.message
